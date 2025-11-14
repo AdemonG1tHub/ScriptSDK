00b1adc007b65062a04e47263f5a7c22f2b49d91
CL TheDreWen <louis.charneux@gmail.com>
2025-11-14 13:30:24 +0100

    Refactor ScriptSDK to enhance event handling and command execution; add support for message processing and waiting data management

--- lib/ScriptSDK.d.ts
+++ lib/ScriptSDK.d.ts
@@ -1,9 +1,11 @@
 export type WaitingData = {
     [key: string]: (result: any) => void;
 };
+export type actions = 'getIp';
 declare class ScriptSDK {
     private waitingData;
     constructor();
+    private logger;
     private generateId;
     private event;
     private send;
--- lib/ScriptSDK.ts
+++ lib/ScriptSDK.ts
@@ -1,18 +1,20 @@
-import { ScriptEventCommandMessageAfterEvent, system } from "@minecraft/server";
+import { ScriptEventCommandMessageAfterEvent, system, world } from "@minecraft/server";
 
 export type WaitingData = {
     [key: string]: (result: any) => void;
 }
 
+export type actions = 'getIp';
+
 class ScriptSDK {
 
-    private waitingData : WaitingData = {};
+    private waitingData: WaitingData = {};
 
     constructor() {
         system.afterEvents.scriptEventReceive.subscribe((e) => this.event(e))
     }
 
-    private generateId(length = 10) : string {
+    private generateId(length = 10): string {
         var result = '';
         var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         var charactersLength = characters.length;
@@ -22,20 +24,37 @@ class ScriptSDK {
         return result;
     }
 
-    private event(e : ScriptEventCommandMessageAfterEvent) {
+    private event(e: ScriptEventCommandMessageAfterEvent) {
         const { id, message, sourceEntity, sourceType } = e;
+
+        const [message_type, message_id] = id.split(':');
+
+        if (message_type == 'scriptsdkresult') {
+            if (Object.keys(this.waitingData).includes(message_id)) {
+                this.waitingData[message_id](message);
+                delete this.waitingData[message_id];
+            }
+        }
     }
 
-    private async send(action : string, hasResult : boolean = false) {
+    private async send(action: actions, body: string = '', hasResult: boolean = false) {
         return new Promise((resolve) => {
 
             const id = this.generateId();
 
-            if(!hasResult) {
-                resolve(null);  
+            world.getDimension('overworld').runCommand(`scriptevent scriptsdk:${id}-${action} ${body}`);
+
+            if (!hasResult) {
+                resolve(null);
+            }
+
+            this.waitingData[id] = (data) => {
+                resolve(data);
             }
         });
     }
+
+
 }
 
 export default new ScriptSDK();
