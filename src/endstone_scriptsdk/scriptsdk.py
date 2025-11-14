290b319068cb53078fec944f4b42763fde092f04
CL TheDreWen <louis.charneux@gmail.com>
2025-11-14 11:26:49 +0100

    Enhance ScriptSDK with build scripts, dependencies, and initial plugin structure

--- src/endstone_scriptsdk/scriptsdk.py
+++ src/endstone_scriptsdk/scriptsdk.py
@@ -1,5 +1,18 @@
 from endstone.plugin import Plugin
+from colorama import Fore
 
 class ScriptSDK(Plugin):
+    api_version = "0.10"
+    authors = ["TheDreWen"]
+    name = "scriptsdk"
+    description = "Use certain Endstone features directly in your Bedrock add-ons through a JavaScript API. ScriptSDK bridges the gap between Minecraft Bedrock add-ons and Endstone plugin capabilities."
+    website = "https://discord.gg/kQU9p4ZHbR"
 
-    pass
+    plugin_mc_prefix = "§e[Script§1SDK] §r"
+    prefix = f"{Fore.YELLOW}[Script{Fore.BLUE}SDK] {Fore.RESET}"
+
+    def on_load(self):
+        pass
+    
+    def on_enable(self):
+        self.register_events(self)
