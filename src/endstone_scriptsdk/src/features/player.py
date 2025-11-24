2b9387e780c75245ce07301c86b50813959ec406
CL TheDreWen <louis.charneux@gmail.com>
2025-11-24 09:05:36 +0100

    Refactor PlayerData.request method to streamline player data retrieval

--- src/endstone_scriptsdk/src/features/player.py
+++ src/endstone_scriptsdk/src/features/player.py
@@ -6,40 +6,18 @@ if typing.TYPE_CHECKING:
 
 class PlayerData:
     @staticmethod
-    def request(handler : "EventHandler", uuid, action, message):
-        match action:
-            case 'getPlayerIp':
-                '''
-                    body: playerName
-                '''
-                player : Player = handler.plugin.server.get_player(message)
-                if not player:
-                    return handler.response(uuid, False, 404, ['player not found']);
-                return handler.response(uuid, True, 200, [player.address.hostname])
-            
-            case 'getPlayerPing':
-                '''
-                    Body: playerName
-                '''
-                player : Player = handler.plugin.server.get_player(message)
-                if not player:
-                    return handler.response(uuid, False, 404, ['player not found']);
-                return handler.response(uuid, True, 200, [str(player.ping)])
-            
-            case 'getPlayerXuid':
-                '''
-                    Body: playerName
-                '''
-                player : Player = handler.plugin.server.get_player(message)
-                if not player:
-                    return handler.response(uuid, False, 404, ['player not found']);
-                return handler.response(uuid, True, 200, [player.xuid])
-            
-            case 'getPlayerOS':
-                '''
-                    Body: playerName
-                '''
-                player : Player = handler.plugin.server.get_player(message)
-                if not player:
-                    return handler.response(uuid, False, 404, ['player not found']);
-                return handler.response(uuid, True, 200, [player.device_os])
+    def request(handler : "EventHandler", uuid : str, action : str, message : str):
+
+        if action.startswith('getPlayer'):
+            player : Player = handler.plugin.server.get_player(message)
+            if not player:
+                return handler.response(uuid, False, 404, ['player not found']);
+            match action:
+                case 'getPlayerIp':
+                    return handler.response(uuid, True, 200, [player.address.hostname])
+                case 'getPlayerPing':
+                    return handler.response(uuid, True, 200, [str(player.ping)])
+                case 'getPlayerXuid':
+                    return handler.response(uuid, True, 200, [player.xuid])
+                case 'getPlayerOS':
+                    return handler.response(uuid, True, 200, [player.device_os])
