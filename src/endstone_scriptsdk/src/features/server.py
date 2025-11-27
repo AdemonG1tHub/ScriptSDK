23961465c044da30f7bd539cbe831c8bb07bb6fe
CL TheDreWen <louis.charneux@gmail.com>
2025-11-27 10:48:26 +0100

    Bump version to 1.5.0 in README.md; update ServerData class structure in server.py

--- src/endstone_scriptsdk/src/features/server.py
+++ src/endstone_scriptsdk/src/features/server.py
@@ -1,3 +1,10 @@
-# class ServerData:
-#     @staticmethod
-#     def request(handler, uuid, action, message):
+import typing
+
+if typing.TYPE_CHECKING:
+    from endstone_scriptsdk.handler import EventHandler
+
+class ServerData:
+    @staticmethod
+    def request(handler : "EventHandler", uuid : str, action, message):
+
+        pass
